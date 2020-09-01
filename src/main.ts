a318da3df1d0f95a9fa3a3c5af90e77bc6e4704b
Olayode Ezekiel <kielsoft@gmail.com>
2020-09-01 19:18:53 +0100

    update dotevn setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import {NestFactory} from '@nestjs/core';
+require("dotenv").config();
+
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
-
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
 bootstrap();
 
