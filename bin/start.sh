dcd8a3817314ce4e8fc9120386cf233eef910b97
Olayode Ezekiel <kielsoft@gmail.com>
2020-09-01 17:42:05 +0100

    adding npm install when repo is updated

--- bin/start.sh
+++ bin/start.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
-cd /home/sdslearn/repositories/stockservice/bin
-touch ./log/service_startup.log
-service=stock_service
+cd $(dirname "$0")
+cd ../
+
+touch ./logs/stock_service.log
+touch ./logs/git_head.log
+
+processIdentity=stockservice/node_modules
 canrestart=1
 
-if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+appPid=$(ps -ef | grep -v grep | grep $processIdentity | awk '{print $2}');
+
+if [ $(cat 'logs/git_head.log') != $(git rev-parse HEAD) ]
+then 
+    echo $(git rev-parse HEAD) > ./logs/git_head.log
+    if [ $appPid ] 
+    then
+        echo "[ $appPid ] killing StockService app to reinstall npm packages..."
+        kill -9 $appPid;
+        echo "StockService killed."
+    fi
+
+    echo "Reloading npm packages..."
+    npm install
+fi
+
+if [ $appPid ] 
 then
-    echo "$service is running" >> ./log/service_startup.log
+    echo "[ $appPid ] StockService is currently running" >> ./logs/stock_service.log
 elif (($canrestart == '1'))
 then
-    echo "$(date) :     $service is started!!!:"  >> ./log/service_startup.log
-    nohup npm run start:prod  >> ./log/service_startup.log 2>&1 &
+    echo "$(date) :     StockService is started!!!:"  >> ./logs/stock_service.log
+    nohup npm run start:prod  >> ./logs/stock_service.log 2>&1 &
 fi
